1aac63d430752726951618707d50e210c341a652
KULDEEP <90776144+kuldeep1A@users.noreply.github.com>
2022-12-24 23:44:09 +0530

    Create config.py

---
+++ Spotify Playlist/config.py
@@ -0,0 +1,5 @@
+# all these need to be changed to valid credentials
+
+# Spotify for Developers credentials, from https://developer.spotify.com/
+SPOTIFY_ID = "YOUR SPOTIFY ID"
+SPOTIFY_SECRET = "YOUR SPOTIFY SECRET ID"
